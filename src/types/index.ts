e0b3e5e2c34406e8d66c9cf97dfc1db48c86fc2c
Michael Gale <michael@michaelgale.dev>
2023-02-05 10:45:42 +1100

    chore: add types for products

--- src/types/index.ts
+++ src/types/index.ts
@@ -18,4 +18,48 @@ declare global {
       embed: string;
     };
   }
+
+  interface ProductVariant {
+    id: number;
+    sku: string;
+    cost: number;
+    price: number;
+    title: string;
+    grams: number;
+    is_enabled: boolean;
+    is_default: boolean;
+    is_available: boolean;
+    options: number[];
+    quantity: number;
+  }
+
+  interface ProductImage {
+    src: string;
+    variant_ids: number[];
+    position: string;
+    is_default: boolean;
+    is_selected_for_publishing: boolean;
+  }
+  
+  interface Product {
+    id: number;
+    title: string;
+    description: string;
+    tags: string[];
+    options: any[];
+    variants: ProductVariant[];
+    images: ProductImage[];
+    created_at: string;
+    updated_at: string;
+    visible: boolean;
+    is_locked: boolean;
+    blueprint_id: number;
+    user_id: number;
+    shop_id: number;
+    print_provider_id: number;
+    print_areas: any[];
+    print_details: any[];
+    sales_channel_properties: any[];
+    twodaydelivery_enabled: boolean;
+  }
 }
