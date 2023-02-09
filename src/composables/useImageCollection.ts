cc42038b2fe5fcdf68b244b2743cbcf3138f66ed
Michael Gale (he/him) <michael@michaelgale.dev>
2023-02-10 10:35:11 +1100

    docs: typo in comments

--- src/composables/useImageCollection.ts
+++ src/composables/useImageCollection.ts
@@ -3,13 +3,13 @@
  *
  * @param   {string[]}  filenames  Array of filenames to fetch. Should be in the format: "filename.jpg|png|etc" 
  *
- * @return  {object[]}             Array of {image, filename} objects 
+ * @return  {object[]}             Array of {src, filename} objects 
  */
 export async function useImageCollection (filenames:string[]) {
   const images = await Promise.allSettled(filenames.map(async (filename) => {
     const image = await $fetch(`/api/images/${filename}`)
     
-    // return url and filename, since result could be in any order
+    // return src and filename, since result could be in any order
     return { filename, src: image.variants[0] }
   })) as { status: 'fulfilled' | 'rejected', value: any, reason: string}[]; 
 
@@ -20,6 +20,6 @@ export async function useImageCollection (filenames:string[]) {
     }
   })
 
-  // Return image url and filename(s)
+  // Return image src and filename(s)
   return images.map((image) => image.value)
 }
