a5033baaa9d8f7137e7fc2b03395f20d4fcd2214
Michael Gale <michael@michaelgale.dev>
2023-02-12 08:57:15 +1100

    feat: add an error page

---
+++ src/error.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps(['error'])
+</script>
+
+<template>
+  <c-section>
+    <h1>Oops - An error occurred!</h1>
+    <p>{{ error.statusCode }} - {{ error.message }}</p>
+    <nuxt-link to="/">← Return home</nuxt-link>
+  </c-section>
+</template>
