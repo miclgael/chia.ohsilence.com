a37b78e30f14d5f5a9b80b6d469a49889a107666
Michael Gale (he/him) <michael@michaelgale.dev>
2023-10-11 11:11:03 +1100

    feat: add more to sizes.vue

--- src/pages/sizes.vue
+++ src/pages/sizes.vue
@@ -1,5 +1,6 @@
 <template>
 <c-section>
+  <h2>AS Colour (Short Sleeve)</h2>
   <table style="border-collapse: collapse;" cellspacing="0" cellpadding="0">
 <tbody>
 <tr>
@@ -103,5 +104,79 @@
 </tr>
 </tbody>
 </table>
+
+<h2>AS Colour (Long Sleeve)</h2>
+
+<table style="border-collapse: collapse;" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td style="width: 77.0px; height: 18.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Helvetica; min-height: 14.0px;">&nbsp;</p>
+</td>
+<td style="width: 77.0px; height: 18.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #414141; font-family: Times; font-size: large;">Width, cm</span></p>
+</td>
+<td style="width: 77.0px; height: 18.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #414141; font-family: Times; font-size: large;">Length, cm</span></p>
+</td>
+</tr>
+<tr>
+<td style="width: 77.0px; height: 17.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #000000; font-family: Times; font-size: large;">S</span></p>
+</td>
+<td style="width: 77.0px; height: 17.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #414141; font-family: Times; font-size: large;">48.89</span></p>
+</td>
+<td style="width: 77.0px; height: 17.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #414141; font-family: Times; font-size: large;">73.00</span></p>
+</td>
+</tr>
+<tr>
+<td style="width: 77.0px; height: 17.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #000000; font-family: Times; font-size: large;">M</span></p>
+</td>
+<td style="width: 77.0px; height: 17.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #414141; font-family: Times; font-size: large;">53.35</span></p>
+</td>
+<td style="width: 77.0px; height: 17.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #414141; font-family: Times; font-size: large;">75.55</span></p>
+</td>
+</tr>
+<tr>
+<td style="width: 77.0px; height: 18.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #000000; font-family: Times; font-size: large;">L</span></p>
+</td>
+<td style="width: 77.0px; height: 18.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #414141; font-family: Times; font-size: large;">57.15</span></p>
+</td>
+<td style="width: 77.0px; height: 18.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #414141; font-family: Times; font-size: large;">78.10</span></p>
+</td>
+</tr>
+<tr>
+<td style="width: 77.0px; height: 17.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #000000; font-family: Times; font-size: large;">XL</span></p>
+</td>
+<td style="width: 77.0px; height: 17.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #414141; font-family: Times; font-size: large;">60.96</span></p>
+</td>
+<td style="width: 77.0px; height: 17.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #414141; font-family: Times; font-size: large;">80.65</span></p>
+</td>
+</tr>
+<tr>
+<td style="width: 77.0px; height: 18.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #000000; font-family: Times; font-size: large;">2XL</span></p>
+</td>
+<td style="width: 77.0px; height: 18.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #414141; font-family: Times; font-size: large;">64.75</span></p>
+</td>
+<td style="width: 77.0px; height: 18.0px; padding: 10.0px 10.0px 10.0px 10.0px; border: 1.0px solid #000000;" valign="middle">
+<p style="margin: 0.0px 0.0px 0.0px 0.0px;"><span style="font: 15.0px Times; font-variant-ligatures: common-ligatures; color: #414141; font-family: Times; font-size: large;">83.20</span></p>
+</td>
+</tr>
+</tbody>
+</table>
+
 </c-section>
 </template>
