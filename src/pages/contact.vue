4143fcb9da736e07f4f7291f4881296f62ab2180
Michael Gale <michael@michaelgale.dev>
2024-05-06 19:50:45 +1000

    chore: remove ig link

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -13,10 +13,9 @@ useHead({
 <template>
   <c-section>
     <h3>Contact</h3>
-    <p>For all enquiries, please email <a href="mailto:&#109;&#105;&#099;&#104;&#097;&#101;&#108;&#064;&#109;&#105;&#099;&#104;&#097;&#101;&#108;&#103;&#097;&#108;&#101;&#046;&#100;&#101;&#118;?subject=re:%20Oh%20Silence">&#109;&#105;&#099;&#104;&#097;&#101;&#108;&#064;&#109;&#105;&#099;&#104;&#097;&#101;&#108;&#103;&#097;&#108;&#101;&#046;&#100;&#101;&#118; &nearr;</a></p>
-    <h3>Follow</h3>
-    <p>
-      For up to date information, please <a href="https://instagram.com/ohsilencerecords">follow us on Instagram &nearr;</a>
-    </p>
+    <p>For all enquiries, please email <a href="mailto:ohsilencerecordco@gmail.com">ohsilencerecordco@gmail.com &nearr;</a></p>
+    <!-- <h3>Follow us</h3> -->
+    <!-- eslint-disable-next-line -->
+    <!-- <p>Follow us on <a href="https://sonomu.club/@ohsilencerecords" target="_blank" rel="noopener noreferrer">Mastodon &nearr;</a></p> -->
   </c-section>
 </template>
