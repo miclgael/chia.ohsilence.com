74028bfb3b4f4e75d8fdb702b2eee9527968ea2c
Michael Gale <michael@michaelgale.dev>
2024-05-08 19:25:11 +1000

    chore: verbose typing

--- src/server/api/releases/many/[ids].get.ts
+++ src/server/api/releases/many/[ids].get.ts
@@ -4,9 +4,9 @@ const { getReleaseById } = useReleases()
 
 export default defineEventHandler(async (event) => {
   // Filename param from path: src/server/api/release/many/[ids].get.ts
-  const ids = event?.context?.params?.ids?.split(',')
+  const ids = event?.context?.params?.ids?.split(',') as OhSilenceReleaseId[]
   
-  return ids.map((releaseId:OhSilenceReleaseId) => {
+  return ids?.map((releaseId:OhSilenceReleaseId) => {
     return getReleaseById(releaseId)
   })
 })
