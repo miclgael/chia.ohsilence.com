c1ba8ceeaae1eb44aaca90c3e8c98b3f9f9cdef3
Michael Gale <michael@michaelgale.dev>
2024-07-25 10:57:11 +1000

    chore: mvp blog post

---
+++ src/pages/blog/[[slug]].vue
@@ -0,0 +1,8 @@
+<template>
+   <ContentList path="/blog" v-slot="{ list }">
+      <div v-for="article in list" :key="article._path">
+        <h2>{{ article.title }}</h2>
+        <p>{{ article.description }}</p>
+      </div>
+    </ContentList>
+</template>
---
+++ src/pages/blog/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <ContentDoc 
+    path="/blog"
+  />
+</template>
